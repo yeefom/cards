442315de493915b116bc2cc3a14023ca8f581e1e
Yifeng Wang <yifeng@yeefom.com>
2018-07-18 23:22:45 -0700

    add cards

---
+++ lib/cards.ex
@@ -0,0 +1,59 @@
+defmodule Cards do
+  @moduledoc """
+    Cards module
+  """
+
+  @doc """
+    Returns a list of strings that represent the deck
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  @doc """
+  returns a hand and the reminder of the deck
+
+  ## Examples
+
+    iex> deck = Cards.create_deck()
+    iex> {hand, _deck} = Cards.deal(deck, 1)
+    iex> hand
+    ["Ace of Spades"]
+
+  """
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+end
